fce2cc0e367b26de26afd2bc4ae8711cf737c7a0
Sumit <sumitkhandegar2@gmail.com>
2024-10-31 13:01:25 +0530

    fix: material UI update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  build: {
-    outDir: 'build' 
-  }
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+  ],
 });
